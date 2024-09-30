b49dfeed1c38a9e633a96bacd50c2a4d65d0d4f5
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-09-30 19:11:05 -0400

    Tolerance Decreases Smoothly during Relapse

--- Assets/_Scripts/EventScripts/PlayerInfo.cs
+++ Assets/_Scripts/EventScripts/PlayerInfo.cs
@@ -17,22 +17,39 @@ public class PlayerInfo : MonoBehaviour, IActor
     // -Dimitri
     [Header("Tolerance Meter Settings")] [SerializeField]
     private float maxTolerance;
+
     [SerializeField] private float currentTolerance;
     [SerializeField] private TolereanceMeter tolereanceMeter;
 
-    private int _relapsesThisLevel;
-    [SerializeField] private int relapsesToLose = 3;
+    /// <summary>
+    /// The number of times the player has relapsed during this level.
+    /// </summary>
+    private int _relapseCount;
+
+    [Tooltip("The number of relapses the player can have before losing the level.")] [SerializeField]
+    private int relapsesToLose = 3;
+
     [Tooltip("What percent is the tolerance meter set to when the player relapses?")] [Range(0, 1)] [SerializeField]
     private float toleranceRelapsePercent = .75f;
+
+    [SerializeField] [Min(0)] private float relapseDuration = 3;
+    private float _currentRelapseDuration;
+
+    private bool _isRelapsing;
 
     /// <summary>
     /// An event that is called when the player relapses.
     /// Used mostly to connect to outside scripts.
     /// </summary>
-    public Action<PlayerInfo> OnRelapse;
+    public Action<PlayerInfo> OnRelapseStart;
+
+    /// <summary>
+    /// An event that is called when the player's relapse ends.
+    /// </summary>
+    public Action<PlayerInfo> OnRelapseEnd;
 
     private InputUserHandler _inputUserHandler;
-    
+
     #region Getters
 
     public GameObject GameObject => gameObject;
@@ -44,6 +61,8 @@ public class PlayerInfo : MonoBehaviour, IActor
     public float MaxTolerance => maxTolerance;
 
     public float CurrentTolerance => currentTolerance;
+
+    public bool IsRelapsing => _isRelapsing;
 
     #endregion
 
@@ -79,11 +98,34 @@ public class PlayerInfo : MonoBehaviour, IActor
         // Update the input users
         _inputUserHandler.UpdateInputUsers();
 
+        // Update the relapse duration
+        UpdateRelapseDuration();
+
         // Prevent the tolerance from going below 0 or above the max value
         ClampTolerance();
 
         if (maxTolerance > 0)
             tolereanceMeter.UpdateToleranceUI(currentTolerance / maxTolerance); // Scale to 0-1
+    }
+
+    private void UpdateRelapseDuration()
+    {
+        // Return if the player isn't relapsing
+        if (!_isRelapsing)
+            return;
+
+        // Increment the relapse duration
+        _currentRelapseDuration += Time.deltaTime;
+
+        // Reset the tolerance meter after a relapse
+        var targetTolerance = maxTolerance * toleranceRelapsePercent;
+        var toleranceDifference = targetTolerance - currentTolerance;
+        var toleranceDifferencePerSecond = toleranceDifference / relapseDuration;
+        ChangeTolerance(toleranceDifferencePerSecond * Time.deltaTime);
+
+        // If the relapse duration is greater than the relapse duration, end the relapse
+        if (_currentRelapseDuration >= relapseDuration)
+            EndRelapse();
     }
 
     private void OnDestroy()
@@ -130,15 +172,22 @@ public class PlayerInfo : MonoBehaviour, IActor
 
         // The player will relapse if the tolerance meter is too high
         if (currentTolerance >= maxTolerance)
-            Relapse();
+            StartRelapse();
     }
 
-    private void Relapse()
+    private void StartRelapse()
     {
-        // Increase the relapse count
-        _relapsesThisLevel++;
+        // Skip if the player is already relapsing
+        if (_isRelapsing)
+            return;
 
-        if (_relapsesThisLevel >= relapsesToLose)
+        // Set the isRelapsing flag to true
+        _isRelapsing = true;
+
+        // Increase the relapse count
+        _relapseCount++;
+
+        if (_relapseCount >= relapsesToLose)
         {
             // The player dies / restarts the level from relapsing too many times!
             DieFromRelapse();
@@ -146,13 +195,23 @@ public class PlayerInfo : MonoBehaviour, IActor
             return;
         }
 
+        // Reset the relapse duration
+        _currentRelapseDuration = 0;
+
         // Invoke the relapse event
-        OnRelapse?.Invoke(this);
-        
-        // Reset the tolerance meter after a relapse
-        var targetTolerance = maxTolerance * toleranceRelapsePercent;
-        var toleranceDifference = targetTolerance - currentTolerance;
-        ChangeTolerance(toleranceDifference);
+        OnRelapseStart?.Invoke(this);
+    }
+
+    private void EndRelapse()
+    {
+        // Set the isRelapsing flag to false
+        _isRelapsing = false;
+
+        // Reset the relapse duration
+        _currentRelapseDuration = 0;
+
+        // Invoke the end relapse event
+        OnRelapseEnd?.Invoke(this);
     }
 
     private void DieFromRelapse()
