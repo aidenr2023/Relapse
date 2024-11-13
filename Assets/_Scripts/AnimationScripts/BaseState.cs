500f0f13d47a1fa20a7e9560698c16af90f56a87
Andre Powell <andre.r.powell@knights.ucf.edu>
2024-11-13 18:53:25 -0500

    Added: StateMachine for Animations

---
+++ Assets/_Scripts/AnimationScripts/BaseState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public abstract class BaseState<EState> where EState : Enum 
+{
+    public BaseState(EState key)
+    {
+        StateKey = key;
+    }
+    
+    public EState StateKey { get; private set; }
+    
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
+    public abstract EState GetNextState();
+    public abstract void OnTriggerEnter(Collider other);
+    public abstract void OnTriggerStay(Collider other);
+    public abstract void OnTriggerExit(Collider other);
+    
+    
+}
