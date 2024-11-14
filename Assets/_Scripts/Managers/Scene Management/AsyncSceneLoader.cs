641840c4159fd6640154d9b66bf9199f4fe2d3ba
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-11-14 02:06:50 -0500

    Scene Disabling Instead of Unloading

--- Assets/_Scripts/Managers/Scene Management/AsyncSceneLoader.cs
+++ Assets/_Scripts/Managers/Scene Management/AsyncSceneLoader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,7 +15,9 @@ public class AsyncSceneLoader : MonoBehaviour
 
     [SerializeField] private SceneLoadInformation scenesToLoadOnAwake;
 
-    private readonly Dictionary<string, AsyncOperation> _asyncOperations = new();
+    private readonly ConcurrentDictionary<string, Scene> _managedScenes = new();
+
+    private readonly Dictionary<string, IEnumerable<GameObject>> _disabledScenes = new();
 
     private void Awake()
     {
@@ -30,19 +34,47 @@ public class AsyncSceneLoader : MonoBehaviour
     {
         Debug.Log($"Loading Scene!: {sceneName} w/ priority: {priority}");
 
-        // Return if the scene is already loaded
-        if (_asyncOperations.ContainsKey(sceneName))
+        if (sceneName == null)
             return;
 
-        // Start loading the scene
-        var op = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        // If the scene already exists in the managed scenes, return
+        if (_managedScenes.ContainsKey(sceneName))
+        {
+            // If the scene is disabled, enable it
+            if (!_disabledScenes.TryGetValue(sceneName, out var disabledSceneObjects))
+                return;
 
-        // Set the priority of the scene's loading operation
-        if (op != null)
-            op.priority = priority;
+            Debug.Log($"Re-Enabling Scene: {sceneName}");
 
-        // Add the scene to the dictionary
-        _asyncOperations.Add(sceneName, op);
+            foreach (var sceneObject in disabledSceneObjects)
+                sceneObject.SetActive(true);
+
+            // Remove the scene from the disabled scenes
+            _disabledScenes.Remove(sceneName);
+
+            return;
+        }
+
+        // Start to async load the scene
+        var asyncOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        // Check if the async operation is null
+        if (asyncOperation == null)
+        {
+            // Log an error
+            Debug.LogError($"Failed to load scene: {sceneName}");
+            return;
+        }
+
+        // Set the priority of the async operation
+        asyncOperation.priority = priority;
+
+        var scene = SceneManager.GetSceneByName(sceneName);
+
+        Debug.Log($"Scene: {scene.name} is loaded: {scene.isLoaded}");
+
+        // Add the scene to the managed scenes
+        _managedScenes.TryAdd(sceneName, scene);
     }
 
     public void LoadScene(SceneLoadInformation.SceneLoadInfoPriority scene)
@@ -55,15 +87,19 @@ public class AsyncSceneLoader : MonoBehaviour
 
     public void LoadSceneNoAsync(string sceneName)
     {
-        // Return if the scene is already loaded
-        if (_asyncOperations.ContainsKey(sceneName))
+        // Check if the scene name is null
+        if (sceneName == null)
             return;
 
-        // Start loading the scene
+        // Check if the scene is already loaded
+        if (_managedScenes.ContainsKey(sceneName))
+            return;
+
+        // Load the scene
         SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
 
-        // Add the scene to the dictionary
-        _asyncOperations.Add(sceneName, null);
+        // Add the scene to the managed scenes
+        _managedScenes.TryAdd(sceneName, SceneManager.GetSceneByName(sceneName));
     }
 
     public void UnloadScene(string sceneName)
@@ -71,17 +107,45 @@ public class AsyncSceneLoader : MonoBehaviour
         if (sceneName == null)
             return;
 
-        // Return if the scene is not loaded
-        if (!_asyncOperations.TryGetValue(sceneName, out var op))
+        // Check if the scene is not in the managed scenes
+        if (!_managedScenes.ContainsKey(sceneName))
             return;
 
-        // // Stop the scene from loading
-        // if (op != null)
-        //     op.allowSceneActivation = false;
+        // Check if the scene has a value
+        if (!_managedScenes.TryGetValue(sceneName, out var scene))
+            return;
 
-        SceneManager.UnloadSceneAsync(sceneName);
+        // Return if the scene is already disabled
+        if (_disabledScenes.ContainsKey(sceneName))
+            return;
 
-        _asyncOperations.Remove(sceneName);
+        // // Unload the scene
+        // SceneManager.UnloadSceneAsync(scene);
+        //
+        // // Remove the scene from the managed scenes
+        // _managedScenes.TryRemove(sceneName, out _);
+
+        var sceneObjects = scene.GetRootGameObjects();
+
+        // Get all the currently active objects in the scene
+        var activeObjects = new List<GameObject>();
+
+        // Disable the scene by disabling all its game objects
+        foreach (var sceneObject in sceneObjects)
+        {
+            // Continue if the scene object is not active
+            if (!sceneObject.activeSelf)
+                continue;
+
+            // Disable the scene object
+            sceneObject.SetActive(false);
+
+            // Add the scene object to the active objects
+            activeObjects.Add(sceneObject);
+        }
+
+        // Add this scene to the disabled scenes
+        _disabledScenes.Add(sceneName, activeObjects);
     }
 
     public void LoadScenesExclusive(SceneLoadInformation sceneLoadInformation)
@@ -89,27 +153,32 @@ public class AsyncSceneLoader : MonoBehaviour
         if (sceneLoadInformation == null)
             return;
 
-        // Keep track of the scenes that need to be loaded
-        var scenesToLoad = new HashSet<SceneLoadInformation.SceneLoadInfoPriority>(sceneLoadInformation.Scenes);
+        // Create a hash set to store all the scenes that need to be unloaded
+        var scenesToUnload = new HashSet<string>(_managedScenes.Keys);
 
-        var scenesToUnload = new HashSet<string>(_asyncOperations.Keys);
-
-        // Remove all scenes that need to be loaded from the scenes to unload
-        foreach (var scene in scenesToLoad)
+        // For each scene in the scene load information, remove the scene from the scenes to unload
+        foreach (var scene in sceneLoadInformation.Scenes)
             scenesToUnload.Remove(scene.SceneName);
 
-        // Unload all scenes
+        // Unload all the scenes that need to be unloaded
         foreach (var scene in scenesToUnload)
             UnloadScene(scene);
 
-        // Load all new scenes
-        foreach (var scene in scenesToLoad)
-        {
-            // Continue if the scene is already loaded
-            if (_asyncOperations.ContainsKey(scene.SceneName))
-                continue;
-
+        // Load all the scenes in the scene load information
+        foreach (var scene in sceneLoadInformation.Scenes)
             LoadScene(scene);
-        }
+    }
+
+    private Scene[] GetAllScenes()
+    {
+        // Get the scene count
+        var sceneCount = SceneManager.sceneCount;
+
+        // Get all scenes
+        var scenes = new Scene[sceneCount];
+        for (var i = 0; i < sceneCount; i++)
+            scenes[i] = SceneManager.GetSceneAt(i);
+
+        return scenes;
     }
 }
