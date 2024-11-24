92ae8ed8af35565187aee07264234f5d357a150e
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-11-24 18:07:20 -0500

    Tooltip Collider

---
+++ Assets/_Scripts/Journal/Tooltip/TooltipCollider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class TooltipCollider : MonoBehaviour
+{
+    [SerializeField] private string tooltipText;
+
+    [SerializeField] private bool destroyOnEnter;
+
+    private Collider _collider;
+
+    private void Awake()
+    {
+        // Get the collider component
+        _collider = GetComponent<Collider>();
+
+        // Assert that the collider is not null
+        if (_collider == null)
+            throw new NullReferenceException("TooltipCollider: Collider is null.");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the other collider is the player
+        if (!other.CompareTag("Player"))
+            return;
+
+        // Show the tooltip
+        JournalTooltipManager.Instance.AddTooltip(tooltipText);
+
+        // Destroy the object if destroyOnEnter is true
+        if (destroyOnEnter)
+            Destroy(gameObject);
+    }
+}
