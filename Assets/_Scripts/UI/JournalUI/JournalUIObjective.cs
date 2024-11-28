d4ae8601f70b5506757efb278b3e8323f284174d
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-11-28 01:05:58 -0500

    Journal Tasks Screen Logic

---
+++ Assets/_Scripts/UI/JournalUI/JournalUIObjective.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class JournalUIObjective : MonoBehaviour
+{
+    [SerializeField] private JournalObjective objective;
+
+    [SerializeField] private Color incompleteColor;
+    [SerializeField] private Color completeColor;
+    [SerializeField] private Image colorImage;
+    [SerializeField] private TMP_Text objectiveText;
+    [SerializeField] private Button button;
+    [SerializeField] private EventTrigger eventTrigger;
+
+    public JournalObjective Objective => objective;
+
+    public Button Button => button;
+
+    public EventTrigger EventTrigger => eventTrigger;
+
+    private void Update()
+    {
+        if (objective == null)
+            throw new Exception("Objective not set");
+
+        // Update the objective data
+        UpdateObjectiveData();
+    }
+
+    private void UpdateObjectiveData()
+    {
+        objectiveText.text = objective.ShortDescription;
+
+        // Set the color of the objective based on its completion status
+        if (JournalObjectiveManager.Instance == null)
+        {
+            // Set the color to the incomplete color
+            colorImage.color = incompleteColor;
+            return;
+        }
+
+        // Set the color
+        var color = incompleteColor;
+
+        if (JournalObjectiveManager.Instance.IsObjectiveComplete(objective))
+            color = completeColor;
+
+        colorImage.color = color;
+    }
+
+    public void SetObjective(JournalObjective obj)
+    {
+        objective = obj;
+    }
+}
