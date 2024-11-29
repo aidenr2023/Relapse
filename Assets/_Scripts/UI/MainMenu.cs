e1b767dc0d48eecf4958d5d15ba4eec5a8033e86
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-11-29 16:53:21 -0500

    Background Loading For Main Menu Script

--- Assets/_Scripts/UI/MainMenu.cs
+++ Assets/_Scripts/UI/MainMenu.cs
@@ -1,12 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    private string _sceneName = "ApartmentBlockout";
+
+    private AsyncOperation _loadSceneOperation;
+
+    private void Awake()
+    {
+        Debug.Log($"Bruh who am I?: {gameObject}");
+    }
+
+    private void Start()
+    {
+        // Asynchronously load the scene
+        _loadSceneOperation = SceneManager.LoadSceneAsync(_sceneName, LoadSceneMode.Single);
+        _loadSceneOperation.allowSceneActivation = false;
+    }
+
     public void StartButton()
     {
-        SceneManager.LoadScene("ApartmentBlockout");
+        // Set the scene to active
+        _loadSceneOperation.allowSceneActivation = true;
     }
     public void ExitButton()
     {
