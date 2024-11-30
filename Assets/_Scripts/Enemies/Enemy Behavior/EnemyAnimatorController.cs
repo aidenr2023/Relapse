136447e7ba68dc5b884ebd845a42fe8aecdd2449
Andre Powell <andre.r.powell@knights.ucf.edu>
2024-11-30 02:46:43 -0500

    Updated: Enemy Walk and Run cycles

--- Assets/_Scripts/Enemies/Enemy Behavior/EnemyAnimatorController.cs
+++ Assets/_Scripts/Enemies/Enemy Behavior/EnemyAnimatorController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAnimatorController : MonoBehaviour
+public class EnemyAnimatorController : StateMachineBehaviour
 {
     // Reference to the NavMeshAgent
     public NavMeshAgent navMeshAgent;
@@ -12,11 +12,22 @@ public class EnemyAnimatorController : MonoBehaviour
     public Animator animator;
 
     // public bool canMove = false;
-
-    void Update()
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // return;
+        // Get the EnemyAnimatorController component from the parent object
+        
+        
+    }
 
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        EnemyMovement();
+        PickRandomState(animator, 3);
+    }
+    
+    private void EnemyMovement()
+    {
         // Get the current speed from the NavMeshAgent
         float speed = navMeshAgent.velocity.magnitude;
 
@@ -26,20 +37,25 @@ public class EnemyAnimatorController : MonoBehaviour
         // Optionally, you can control other NPC behaviors or animations here as well
         animator.speed = navMeshAgent.speed / 3f;
     }
+    
+    
+    
+    //from substate machine pick random state
+    public void PickRandomState(Animator animator, int numberOfStates)
+    {
+        if (numberOfStates <= 0)
+        {
+            Debug.LogWarning("Number of states must be greater than 0.");
+            return;
+        }
 
-    // public void DisableMovement()
-    // {
-    //     canMove = false;
-    //     navMeshAgent.enabled = false;
-    //     //navMeshAgent.isStopped = true;
-    //    // animator.SetFloat("Speed", 0);
-    // }
-    //
-    // public void EnableMovement()
-    // {
-    //     canMove = true;
-    //     navMeshAgent.enabled = true;
-    //     //navMeshAgent.isStopped = false;
-    //     animator.SetFloat("Speed", 2);
-    // }
+        // Pick a random number between 0 and (numberOfStates - 1)
+        int randomState = Random.Range(0, numberOfStates);
+
+        // Set the Animator parameter to the random state
+        animator.SetInteger("RandomState", randomState);
+
+        Debug.Log($"Picked Random State: {randomState}");
+    }
+    
 }
