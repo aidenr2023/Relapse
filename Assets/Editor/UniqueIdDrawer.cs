b8beb624318298f8e0d2adc089d1503e0c9b7436
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-01-02 00:12:19 -0500

    Scene Loading Data Serialization Pt.2.3
    Multi-object editing to make my life easier

--- Assets/Editor/UniqueIdDrawer.cs
+++ Assets/Editor/UniqueIdDrawer.cs
@@ -5,37 +5,129 @@
 using UnityEditor;
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 
 // Place this file inside Assets/Editor
 [CustomPropertyDrawer(typeof(UniqueIdentifierAttribute))]
 public class UniqueIdentifierDrawer : PropertyDrawer
 {
+    private const int BUTTON_HEIGHT = 18;
+
     public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
     {
+        // Check if the property is being multi-edited
+        var isMultiEditing = prop.serializedObject.isEditingMultipleObjects;
+
         // Check if the associated game object is a prefab
         // Check if the inspector is prefab mode in context
         var isPrefab = PrefabUtility.IsPartOfPrefabAsset(prop.serializedObject.targetObject) ||
                        PrefabStageUtility.GetCurrentPrefabStage() != null;
 
         // Generate a unique ID, defaults to an empty string if nothing has been serialized yet
-        if (prop.stringValue == "" && !isPrefab)
-        {
-            var guid = Guid.NewGuid();
-            prop.stringValue = guid.ToString();
-        }
+        if (prop.stringValue == "" && !isPrefab && !isMultiEditing)
+            GenerateNewId(prop);
         else if (isPrefab)
             prop.stringValue = "";
 
+
+        // If the property is not being multi-edited, draw the label field
         // Place a label so it can't be edited by accident
-        var textFieldPosition = position;
-        textFieldPosition.height = 16;
-        DrawLabelField(textFieldPosition, prop, label);
+        if (!isMultiEditing)
+        {
+            var textFieldPosition = position;
+            textFieldPosition.height = 16;
+
+            DrawLabelField(textFieldPosition, prop, label);
+        }
+
+        // If it is being multi-edited
+        else if (!isPrefab)
+        {
+            // EditorGUI.LabelField(textFieldPosition, label, new GUIContent("MULTI-EDITING MODE"));
+
+            var isMultipleValuesTheSame = false;
+
+            var uniqueValues = new HashSet<string>();
+
+            // Check if any of the targets have the same value
+            foreach (var target in prop.serializedObject.targetObjects)
+            {
+                var so = new SerializedObject(target);
+                var sp = so.FindProperty(prop.propertyPath);
+
+                if (uniqueValues.Contains(sp.stringValue))
+                {
+                    isMultipleValuesTheSame = true;
+                    break;
+                }
+
+                uniqueValues.Add(sp.stringValue);
+            }
+
+            var warningRect = new Rect(position.x, position.y, position.width, BUTTON_HEIGHT);
+
+            // If any values are the same, tell the user to generate new IDs
+            if (isMultipleValuesTheSame)
+            {
+                EditorGUI.HelpBox(
+                    warningRect,
+                    "Multiple objects have the same ID. Press Generate new IDs!",
+                    MessageType.Error
+                );
+            }
+            else
+            {
+                EditorGUI.HelpBox(
+                    warningRect,
+                    "All objects have unique IDs.",
+                    MessageType.Info
+                );
+            }
+        }
+
+        // Make a new button that says "Generate New ID"
+        var buttonRect = new Rect(position.x, position.y + 18, position.width, BUTTON_HEIGHT);
+
+        // Get the height of the button and add it to the property height
+        position.height += BUTTON_HEIGHT;
+
+        if (GUI.Button(buttonRect, "Generate New ID") && !isPrefab)
+            GenerateNewId(prop);
     }
 
     private void DrawLabelField(Rect position, SerializedProperty prop, GUIContent label)
     {
         EditorGUI.LabelField(position, label, new GUIContent(prop.stringValue));
+    }
+
+    public override float GetPropertyHeight(SerializedProperty prop, GUIContent label)
+    {
+        return base.GetPropertyHeight(prop, label) + BUTTON_HEIGHT;
+    }
+
+    private void GenerateNewId(SerializedProperty prop)
+    {
+        // Check if multiple objects are being edited
+        var isMultiEditing = prop.serializedObject.isEditingMultipleObjects;
+
+        // For each object being edited, generate a new GUID
+        if (isMultiEditing)
+        {
+            foreach (var target in prop.serializedObject.targetObjects)
+            {
+                var serializedObject = new SerializedObject(target);
+                var serializedProperty = serializedObject.FindProperty(prop.propertyPath);
+
+                GenerateNewId(serializedProperty);
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            return;
+        }
+
+        var guid = Guid.NewGuid();
+        prop.stringValue = guid.ToString();
     }
 }
 
