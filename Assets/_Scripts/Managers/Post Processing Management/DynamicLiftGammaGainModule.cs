1e933a3e1c725e93605d5a7414fc47df4aef8eca
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-01-10 17:05:41 -0500

    Brighter Post-Processing

---
+++ Assets/_Scripts/Managers/Post Processing Management/DynamicLiftGammaGainModule.cs
@@ -0,0 +1,18 @@
+﻿public class DynamicLiftGammaGainModule : DynamicPostProcessingModule
+{
+    #region Private Fields
+
+    #endregion
+
+    protected override void CustomInitialize(DynamicPostProcessVolume controller)
+    {
+    }
+
+    public override void Start()
+    {
+    }
+
+    public override void Update()
+    {
+    }
+}
