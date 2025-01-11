7b6c24abf34c765f6ab51acf5355c281fcd58a67
Andre Powell <andre.r.powell@knights.ucf.edu>
2025-01-11 14:30:58 -0500

    Added Reloading Animation and temp arms

---
+++ Assets/SyncReload.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncReload : MonoBehaviour
+{
+   //get the animator of the player body
+   [SerializeField] private Animator _player_animator;
+
+
+   public void SyncReloadWithPlayer()
+   {
+      _player_animator.SetTrigger("Reload");
+   }
+}
