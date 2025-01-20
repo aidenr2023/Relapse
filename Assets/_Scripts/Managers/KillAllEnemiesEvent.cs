5f41681f193454b769edffbed5e47a3f0faea7ea
MikelSienra <mi355812@ucf.edu>
2025-01-20 00:03:49 -0500

    Mikel combat garden

--- Assets/_Scripts/Managers/KillAllEnemiesEvent.cs
+++ Assets/_Scripts/Managers/KillAllEnemiesEvent.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class TutorialAnimation : MonoBehaviour
+public class CheckEnemiesPlayAnimation : MonoBehaviour
 {
     public GameObject[] enemies;
 
