c3eacce55822f549fa334508aca09cf198029f20
Brian <179760529+Brian1657@users.noreply.github.com>
2025-01-20 03:05:19 -0500

    Pause UI refined

--- Assets/CraneTrigger.cs
+++ Assets/CraneTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CraneTrigger : MonoBehaviour
 {
-    public Animation anim;
+    public Animator anim;
     // Start is called before the first frame update
     void Awake()
     {
