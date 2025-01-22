59ec58ea61a01199afcefbbf803a6bfb26356e4e
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-01-22 01:08:12 -0500

    Custom Trigger Script

---
+++ Assets/_Scripts/Util/CustomTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CustomTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onTriggerEnter;
+    [SerializeField] private UnityEvent onTriggerExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Return if the other object is not the player
+        if (!other.CompareTag("Player"))
+            return;
+
+        onTriggerEnter.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Return if the other object is not the player
+        if (!other.CompareTag("Player"))
+            return;
+
+        onTriggerExit.Invoke();
+    }
+}
