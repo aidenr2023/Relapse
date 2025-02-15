c3ab4cbe2eb5ae7b3c5d14dd06164404eaf1f456
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-02-15 13:24:54 -0500

    Made Dimitri Scene Cooler

---
+++ Assets/_Scripts/Util/LightHelper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LightHelper : MonoBehaviour
+{
+    [SerializeField] private Light[] lights;
+
+    public void ChangeColor(string hex)
+    {
+        var trimmedHex = hex.Trim();
+
+        if (trimmedHex[0] != '#')
+            hex = "#" + trimmedHex;
+
+        // Convert the hex string to a color
+        ColorUtility.TryParseHtmlString(hex, out var color);
+
+        foreach (var cLight in lights)
+        {
+            // Continue if the light is null
+            if (cLight == null)
+                continue;
+
+            // Set the color of the light
+            cLight.color = color;
+        }
+    }
+}
