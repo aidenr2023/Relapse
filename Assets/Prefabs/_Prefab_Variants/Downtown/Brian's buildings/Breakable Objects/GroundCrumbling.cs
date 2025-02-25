404c9d455123fad2bb09f687eab54b1ef7f01278
Brian <179760529+Brian1657@users.noreply.github.com>
2025-02-25 01:50:18 -0500

    andre is helping

---
+++ Assets/Prefabs/_Prefab_Variants/Downtown/Brian's buildings/Breakable Objects/GroundCrumbling.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GroundCrumbling : MonoBehaviour
+{
+    //reference to the timeline component
+    [SerializeField] PlayableAsset crumble;
+   
+   //floor object that is shrunk to a invisible state
+   [SerializeField] private GameObject floor;
+   
+   //delay before the floor animation is reversed
+   [SerializeField] private float delay = 3f;
+   
+   static string isTriggered = "isTriggered";
+    // Start is called before the first frame update
+    void Awake()
+    {
+    }
+
+    // as the player enters the trigger, the floor will play an animation
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="Player")
+        {
+               
+               //floor.SetActive(false);
+        }
+    }
+    
+    //once the player exits the trigger, the floor will play a reverse animation
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag =="Player")
+        {
+            StartCoroutine(Reverse());
+        }
+    }
+    
+    //turn off the floor object function (optional)
+    public void Turnoff()
+    {
+        floor.SetActive(false);
+    }
+
+    //turn on the floor object function (optional)
+    public void Turnon()
+    {
+        floor.SetActive(true);
+    }
+    
+    // Reverse the animation
+    IEnumerator Reverse()
+    {
+        yield return new WaitForSeconds(delay);
+        //floor.SetActive(true);
+        
+        
+    }
+}
