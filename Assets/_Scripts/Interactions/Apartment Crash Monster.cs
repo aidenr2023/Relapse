feebc9c4d606d91b027f75ed47af2f29a4c792fe
Brian <179760529+Brian1657@users.noreply.github.com>
2025-03-03 23:52:32 -0500

    movement 2 set dress

---
+++ Assets/_Scripts/Interactions/Apartment Crash Monster.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApartmentCrashMonster : MonoBehaviour
+{
+    [SerializeField] public GameObject enemy;
+
+    [SerializeField] public float delay = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(3f);
+        enemy.SetActive(true);
+    }
+}
