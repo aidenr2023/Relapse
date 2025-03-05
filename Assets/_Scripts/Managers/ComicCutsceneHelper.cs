b8906064cf3d8a6f752a91ac4a531cfb6a066027
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-03-05 01:31:02 -0500

    Menu + Test Comic Cutscene

---
+++ Assets/_Scripts/Managers/ComicCutsceneHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class ComicCutsceneHelper : MonoBehaviour
+{
+    [SerializeField, Range(0, 1)] private float timeScale = 1;
+
+    private void Update()
+    {
+        // Set the time scale to the desired time scale
+        Time.timeScale = timeScale;
+    }
+    
+    public void SetTimeScale(float newTimeScale)
+    {
+        // Set the time scale to the new time scale
+        timeScale = newTimeScale;
+    }
+}
