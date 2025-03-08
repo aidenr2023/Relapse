1db22b59e5d5312faa0a70982a9a30c33dd24cee
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-03-08 01:06:58 -0500

    New Enemy Behavior Started

---
+++ Assets/_Scripts/Enemies/New Enemy Behavior/IBehaviorAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public interface IBehaviorAction
+{
+    public float Weight { get; }
+    
+    public void Start(NewEnemyBehaviorBrain brain);
+}
+
+[Serializable]
+public struct BehaviorActionMove : IBehaviorAction
+{
+    public MoveAction moveAction;
+    public float weight;
+    
+    public float minCooldown;
+    public float maxCooldown;
+    
+    public float Weight => weight;
+    
+    public void Start(NewEnemyBehaviorBrain brain)
+    {
+    }
+
+    public enum MoveAction
+    {
+        Idle,
+        
+        StrafeLeft,
+        StrafeRight,
+        StrafeForward,
+        StrafeBackward,
+        
+        MoveTowardTarget,
+        MoveAwayFromTarget,
+        
+        MovementScript,
+    }
+}
