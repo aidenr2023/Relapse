948fa0f35c5ad05cca35dcd5eec23cf5538e2f4f
Brian <179760529+Brian1657@users.noreply.github.com>
2025-03-11 23:44:25 -0400

    here dimitri

---
+++ Assets/_Scripts/Interactions/Phase 2 transition.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Phase2transition : MonoBehaviour
+{
+    [SerializeField] Scene currentScene;
+    [SerializeField] Scene nextScene;
+    [SerializeField] Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Transition ()
+    {
+        
+    }
+}
