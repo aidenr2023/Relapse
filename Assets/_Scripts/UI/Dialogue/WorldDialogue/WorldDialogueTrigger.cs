c6e2126d66f0a439c43e1fd857bc4ed931e097c7
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-03-11 22:52:01 -0400

    World Dialogue UI Script + Trigger

--- Assets/_Scripts/UI/Dialogue/WorldDialogue/WorldDialogueTrigger.cs
+++ Assets/_Scripts/UI/Dialogue/WorldDialogue/WorldDialogueTrigger.cs
@@ -1,6 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class WorldDialogueTrigger : MonoBehaviour
 {
+    [SerializeField] private WorldDialogue worldDialogue;
     [SerializeField] private bool activateOnce;
+
+    [SerializeField] private UnityEvent onTriggerEnter;
+    
+    private int _timesActivated;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Return if the other object is not the player
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (activateOnce && _timesActivated > 0)
+            return;
+
+        // Increment the times activated
+        _timesActivated++;
+
+        // Activate the dialogue
+        WorldDialogueUI.StartDialogue(worldDialogue);
+        
+        // On trigger enter event
+        onTriggerEnter.Invoke();
+    }
 }
