96807fb1bfa4eff330aebc4c681d20152fefe006
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-03-23 05:12:35 -0400

    Boss Chain Lightning (All 4 Powers Done)

---
+++ Assets/_Scripts/Enemies/Boss Powers/BossChainLightningBehavior.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BossChainLightningBehavior : BossPowerBehavior
+{
+    #region Serialized Fields
+
+    [SerializeField] private BossChainLightningProjectile projectilePrefab;
+    [SerializeField] private Transform[] lightningPoints;
+
+    [SerializeField] private float lifeTime = 5;
+
+    [field: SerializeField, Min(0)] public float Damage { get; private set; } = 20f;
+    
+    [field: SerializeField, Min(0)] public float MoveDelay { get; private set; } = 1 / 8f;
+    [field: SerializeField, Min(0)] public float MaxMoveDistance { get; private set; } = 4f;
+    [field: SerializeField, Min(0)] public float MaxMoveAngle { get; private set; } = 15;
+
+    #endregion
+
+    #region Private Fields
+
+    private readonly Dictionary<Transform, BossChainLightningProjectile> _projectiles = new();
+
+    #endregion
+
+    protected override void CustomInitialize(BossEnemyAttack bossEnemyAttack)
+    {
+    }
+
+    protected override IEnumerator CustomUsePower()
+    {
+        // Create the projectiles
+        yield return StartCoroutine(CreateProjectiles());
+
+        // Wait for a second
+        yield return new WaitForSeconds(1);
+
+        // Shoot the projectiles
+        yield return StartCoroutine(ShootProjectiles());
+
+        yield return null;
+    }
+
+    private IEnumerator CreateProjectiles()
+    {
+        // Create an array in a random order of the lightning points
+        var randomLightningPoints = new List<Transform>(lightningPoints);
+
+        // Shuffle the array
+        for (var i = 0; i < randomLightningPoints.Count; i++)
+        {
+            var temp = randomLightningPoints[i];
+            var randomIndex = Random.Range(i, randomLightningPoints.Count);
+            randomLightningPoints[i] = randomLightningPoints[randomIndex];
+            randomLightningPoints[randomIndex] = temp;
+        }
+
+        var coroutines = new Dictionary<Transform, Coroutine>();
+
+        var targetTransform = BossEnemyAttack.Enemy.DetectionBehavior.Target.GameObject.transform;
+        
+        // For each lightning point
+        foreach (var point in randomLightningPoints)
+        {
+            // Instantiate the chain lightning projectile
+            var projectile = Instantiate(projectilePrefab, point);
+            _projectiles[point] = projectile;
+
+            // Create the projectile
+            coroutines[point] = StartCoroutine(projectile.CreateProjectile(this, 0, targetTransform));
+
+            // Wait a little to create the next projectile
+            yield return new WaitForSeconds(1 / 8f);
+        }
+
+        // Yield while there are any projectiles that are not done creating yet
+        yield return new WaitWhile(() => coroutines.Keys.Any(point => !_projectiles[point].IsDoneBeingCreated));
+
+        // Log that the routine is done
+        Debug.Log("Done creating projectiles");
+    }
+
+    private IEnumerator ShootProjectiles()
+    {
+        // Create an array in a random order of the lightning points
+        var randomLightningPoints = new List<Transform>(lightningPoints);
+
+        // Shuffle the array
+        for (var i = 0; i < randomLightningPoints.Count; i++)
+        {
+            var temp = randomLightningPoints[i];
+            var randomIndex = Random.Range(i, randomLightningPoints.Count);
+            randomLightningPoints[i] = randomLightningPoints[randomIndex];
+            randomLightningPoints[randomIndex] = temp;
+        }
+
+        // For each projectile
+        foreach (var point in randomLightningPoints)
+        {
+            // Get the corresponding projectile
+            var projectile = _projectiles[point];
+            
+            // Set the projectile to destroy after a certain amount of time
+            Destroy(projectile.gameObject, lifeTime);
+            
+            // Shoot the projectile
+            yield return StartCoroutine(projectile.ShootProjectile());
+        }
+
+        yield return null;
+    }
+}
---
+++ Assets/_Scripts/Enemies/Boss Powers/BossChainLightningProjectile.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossChainLightningProjectile : MonoBehaviour
+{
+    private BossChainLightningBehavior _chainLightningBehavior;
+    private Transform _target;
+
+    public bool IsDoneBeingCreated { get; private set; }
+
+    private bool _isActive;
+
+
+    public IEnumerator CreateProjectile(BossChainLightningBehavior chainLightningBehavior, float time, Transform target)
+    {
+        // Set the chain lightning behavior
+        _chainLightningBehavior = chainLightningBehavior;
+
+        // Set the target
+        _target = target;
+
+        yield return new WaitForSeconds(time);
+
+        // Set the is done being created flag to true
+        IsDoneBeingCreated = true;
+    }
+
+    public IEnumerator ShootProjectile()
+    {
+        // Set the isActive flag to true
+        _isActive = true;
+
+        // Set the parent of the projectile to null
+        transform.SetParent(null);
+
+        // Start the update coroutine
+        StartCoroutine(UpdateCoroutine());
+
+        yield return null;
+    }
+
+    private IEnumerator UpdateCoroutine()
+    {
+        while (true)
+        {
+            // Move forward
+            transform.position += transform.forward *
+                                  (_chainLightningBehavior.MaxMoveDistance * _chainLightningBehavior.MoveDelay);
+
+            // Then change directions
+
+            // First, set the forward to the direction of the target
+            transform.forward = _target.position - transform.position;
+
+            // Then create a random euler angle
+            var euler = new Vector3(
+                UnityEngine.Random.Range(-_chainLightningBehavior.MaxMoveAngle, _chainLightningBehavior.MaxMoveAngle),
+                UnityEngine.Random.Range(-_chainLightningBehavior.MaxMoveAngle, _chainLightningBehavior.MaxMoveAngle),
+                0
+            );
+
+            // Apply the euler angles to the forward direction
+            transform.forward = Quaternion.Euler(euler) * transform.forward;
+
+            // Then wait
+            yield return new WaitForSeconds(_chainLightningBehavior.MoveDelay);
+        }
+
+        yield return null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Return if not active
+        if (!_isActive)
+            return;
+
+        // Return if the other collider is the shooter
+        if (other.TryGetComponentInParent(out IEnemyAttackBehavior attackBehavior) &&
+            (BossEnemyAttack)attackBehavior == _chainLightningBehavior.BossEnemyAttack)
+            return;
+
+        var hasActor = other.TryGetComponentInParent(out IActor actor);
+
+        // Return if the actor is the shooter
+        if (actor as EnemyInfo == _chainLightningBehavior.BossEnemyAttack.Enemy.EnemyInfo)
+            return;
+
+        // Return if the actor is an enemy
+        if (actor is EnemyInfo)
+            return;
+
+        // Return if the other collider is not an actor
+        if (!hasActor)
+            return;
+
+        Destroy(gameObject);
+        
+        // Damage the player
+        actor.ChangeHealth(
+            -_chainLightningBehavior.Damage,
+            actor,
+            _chainLightningBehavior.BossEnemyAttack,
+            transform.position
+        );
+    }
+}
