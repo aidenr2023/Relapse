435802d66a07292d529f9b509aff5efd866533e8
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-03-24 18:14:42 -0400

    AudioSourceHelper

---
+++ Assets/_Scripts/Sound/AudioSourceHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioSourceHelper : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        // Get the AudioSource component
+        _audioSource = GetComponent<AudioSource>();
+    }
+    
+    public void PlayAudio()
+    {
+        // Play the audio
+        _audioSource.Play();
+    }
+}
