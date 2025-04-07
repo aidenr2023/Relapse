8de43b4d928b21d5aa7b20a13fe2bec483b2173d
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-04-07 17:15:47 -0400

    Custom Interaction Interact Once Checkbox

--- Assets/_Scripts/Util/CustomInteraction.cs
+++ Assets/_Scripts/Util/CustomInteraction.cs
@@ -9,25 +9,29 @@ public class CustomInteraction : MonoBehaviour, IInteractable
 
     [SerializeField] private string interactText = "Interact";
 
+    [SerializeField] private bool interactOnce;
+
     [SerializeField] private UnityEvent OnInteract;
 
     [SerializeField] private InteractionIcon interactionIcon;
 
     #endregion
 
+    private bool _hasInteracted;
+
     #region Getters
 
     public InteractableMaterialManager InteractableMaterialManager { get; set; }
-    
+
     public GameObject GameObject => gameObject;
-    public bool IsInteractable => true;
+    public bool IsInteractable => (interactOnce && !_hasInteracted) || !interactOnce;
 
     public bool HasOutline { get; set; }
 
     public HashSet<Material> OutlineMaterials { get; } = new();
 
     public InteractionIcon InteractionIcon => interactionIcon;
-    
+
     public UnityEvent OnInteraction => OnInteract;
 
     #endregion
@@ -36,7 +40,9 @@ public class CustomInteraction : MonoBehaviour, IInteractable
     {
         // Invoke the event
         OnInteract.Invoke();
-        Debug.Log ("Event activated");
+        
+        // Set the interacted flag
+        _hasInteracted = true;
     }
 
     public void LookAtUpdate(PlayerInteraction playerInteraction)
