0453bc3c705d979519179bcf7c9ff868f1a1c6dd
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-04-08 23:07:19 -0400

    Speedrun Mode Scripts & Prefabs

---
+++ Assets/_Scripts/Managers/SpeedRunModeManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SpeedRunModeManager : MonoBehaviour
+{
+    [SerializeField] private FloatVariable speedRunModeBestTime;
+    [SerializeField] private FloatVariable speedRunModeCurrentTime;
+
+    [SerializeField] private UnityEvent<float> onNewBestTime;
+    [SerializeField] private UnityEvent<float> onNoNewBestTime;
+
+    private bool _isRunning = false;
+
+    private void Update()
+    {
+        if (!_isRunning)
+            return;
+
+        // Increment the current time by the time since the last frame
+        speedRunModeCurrentTime.value += Time.deltaTime;
+    }
+
+    public void RestartTime()
+    {
+        // Set the current time's value to 0
+        speedRunModeCurrentTime.value = 0;
+
+        // Set the isRunning variable to true
+        _isRunning = true;
+    }
+
+    public void StopTime()
+    {
+        // If the clock wasn't running, return
+        if (!_isRunning)
+            return;
+        
+        // Set the isRunning variable to false
+        _isRunning = false;
+
+        // Check if the current time is less than the best time
+        // Set the best time to the current time
+        if (speedRunModeCurrentTime.value < speedRunModeBestTime.value || speedRunModeBestTime.value <= 0)
+        {
+            speedRunModeBestTime.value = speedRunModeCurrentTime.value;
+
+            // Invoke the event
+            onNewBestTime.Invoke(speedRunModeBestTime.value);
+        }
+
+        // Invoke the event
+        else
+            onNoNewBestTime.Invoke(speedRunModeBestTime.value);
+    }
+}
---
+++ Assets/_Scripts/UI/SpeedRunModeTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SpeedRunModeTimer : MonoBehaviour
+{
+    [SerializeField] private string bestTimeTextString = "Best Time: ";
+    [SerializeField] private string currentTimeTextString = "Current Time: ";
+    
+    [Space, SerializeField] private TMP_Text bestTimeText;
+    [SerializeField] private TMP_Text currentTimeText;
+    
+    [Space, SerializeField] private FloatVariable speedRunModeBestTime;
+    [SerializeField] private FloatVariable speedRunModeCurrentTime;
+    
+    private void SetTimes(float bestTime, float currentTime)
+    {
+        // Set the text of the best time and current time
+        bestTimeText.text = $"{bestTimeTextString}{SecondsToTimeString(bestTime)}";
+        currentTimeText.text = $"{currentTimeTextString}{SecondsToTimeString(currentTime)}";
+    }
+
+    private void Update()
+    {
+        // Set the times if the variables are set
+        SetTimes(speedRunModeBestTime.value, speedRunModeCurrentTime.value);
+    }
+    
+    private static string SecondsToTimeString(float time)
+    {
+        // Convert the time to a TimeSpan
+        var timeSpan = TimeSpan.FromSeconds(time);
+        
+        // Return the time as a string
+        return $"{timeSpan.Minutes:00}:{timeSpan.Seconds + (timeSpan.Milliseconds / 1000f):00.00}";
+    }
+}
