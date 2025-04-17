9fb5cf68f93bd5036b865d8308122e0cf03c8e7b
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-04-17 03:48:57 -0400

    ForceSceneLoadHelper script

---
+++ Assets/_Scripts/Util/ForceSceneLoadHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ForceSceneLoadHelper : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    
+    public void ForceChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
